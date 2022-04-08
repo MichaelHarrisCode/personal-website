cfeb72faa38e3e7c5819c52209dade7c417278f0
Michael Harris <46806666+theTerrance@users.noreply.github.com>
2022-04-08 14:52:32 -0700

    Add files via upload
    Small Changes
    
    Randomized animated gradient background
    "About Me" moved to Welcome page
    Titles renamed
    etc

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,31 @@
-function leavingSite() {
-	return confirm("YOU ARE LEAVING THIS WEBSITE.\nDo you want to continue?");
+// Random Number
+// Max is non inclusive
+function randomNumber(max) {
+	return Math.ceil(Math.random() * max) - 1;
 }
+
+// Sets background to a gradient with a random color
+function randomGradient() {
+	let darks = [
+		"darkblue",
+		"red",
+		"maroon"
+	];
+
+	let lights = [
+		"lightblue",
+		"blue",
+		"orange"
+	];
+
+	let num = randomNumber(darks.length);
+
+	document.body.style.background = 'linear-gradient(to bottom right,' + darks[num] + ',' + lights[num] + ')';
+	document.body.style['background-size'] = "200% 200%";
+	document.body.style['background-attachment'] = "fixed";
+}
+
+// When window opens, setup the background
+document.addEventListener('DOMContentLoaded', (event) => {
+    randomGradient();
+});
